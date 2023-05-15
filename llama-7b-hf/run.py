aa2bb0060f97348d1e04cd68e1fc9c25ad17414c
ohm <98101484+ohmreborn@users.noreply.github.com>
2023-05-15 12:57:12 +0700

    Add files via upload

--- llama-7b-hf/run.py
+++ llama-7b-hf/run.py
@@ -2,20 +2,22 @@ import argparse
 import gdown
 from finetune import train
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_path', type=str)
+parser.add_argument('--data_path', type=str, default="output.jsonl")
+parser.add_argument('--wandb_project', type=str, default="huggyllama/llama-7b")
+parser.add_argument('--wandb_log_model', type=str, default="true")
+parser.add_argument('--wandb_run_name', type=str, default="finetune_3_epoch")
 args = parser.parse_args()
 print(args.data_path)
 
 #load_dataset
 url = 'https://drive.google.com/uc?export=download&id=1jbbUtwgwoSQgGnXxzTh-nMReVzEU7ZTU&confirm=t&uuid=d79e2e78-51de-466f-9ceb-3944606141a2&at=AKKF8vwcgi95TGSnSQUNCKx4NTqS:1682865249145'
-output = 'output.jsonl'
-gdown.download(url, output, quiet=False)
+gdown.download(url,output='output.jsonl', quiet=False)
 
 kwargs = {
     "data_path":args.data_path,
-    "wandb_project":"huggyllama/llama-7b",
-    "wandb_log_model":"true",
-    "wandb_run_name":"finetune_3_epoch"
+    "wandb_project":args.wandb_project,
+    "wandb_log_model":args.wandb_log_model,
+    "wandb_run_name":args.wandb_run_name
 }
 
 data = train(**kwargs)
